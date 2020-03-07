4d75f77dd0b122b9fe61b179196d2c280ebabc49
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-07 13:02:48 +0530

    With Cross cutting logic

--- AroundAdvicePgmApproch/src/com/aa/aspect/LoggingAdvice.java
+++ AroundAdvicePgmApproch/src/com/aa/aspect/LoggingAdvice.java
@@ -1,5 +1,32 @@
 package com.aa.aspect;
 
-public class LoggingAdvice {
-	
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class LoggingAdvice implements MethodInterceptor {
+
+	@Override
+	public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+		String methodName = null;
+		Object[] args = null;
+
+		methodName = methodInvocation.getMethod().getName();
+		args = methodInvocation.getArguments();
+
+		for (int i = 0; i < args.length; i++) {
+			if (i == 0) {
+				System.out.println("Logging Information:");
+				System.out.println("Method & Parameters passed are: " + methodName + "(" + args[i]);
+				continue;
+			}
+			System.out.print("," + args[i] + ")");
+		}
+		
+		Object ret = methodInvocation.proceed();
+		
+		System.out.println("Logging report Successful for method: "+methodName+"()");
+		
+		return ret;
+	}
+
 }
